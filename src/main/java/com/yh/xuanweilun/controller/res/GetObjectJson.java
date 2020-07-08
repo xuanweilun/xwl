a0874e223a8d8624bc8e8d3103ffc45a15154e5e
禤为论 <xuanweilun@sinovatech.com>
2020-07-08 11:01:44 +0800

    update old files

--- src/main/java/com/yh/xuanweilun/controller/res/GetObjectJson.java
+++ src/main/java/com/yh/xuanweilun/controller/res/GetObjectJson.java
@@ -30,9 +30,9 @@ public class GetObjectJson {
 //        sb.append("let ");
 //        sb.append(name);
 //        sb.append(" = ");
-        sb.append("{\n");
+        sb.append("{");
         for (Field declaredField : declaredFields) {
-            sb.append("\t");
+            sb.append(" ");
             sb.append(declaredField.getName());
             sb.append(": ");
             if(declaredField.getType().toString().equals("boolean")){
@@ -40,14 +40,48 @@ public class GetObjectJson {
             }else{
                 sb.append("''");
             }
-            sb.append(",\n");
+            sb.append(",");
         }
         sb.append("}}");
-        System.out.println("var vueSave = new Vue({el: '#save',data:"+sb+" );");
-        System.out.println("var vueCheck = new Vue({el: '#check',data: "+sb+");");
-        System.out.println("var vueEdit = new Vue({el: '#edit',data: "+sb+");");
+        System.out.println("");
+        System.out.println("let vueSave = new Vue({el: '#save',data:"+sb+");");
+        System.out.println("let vueEdit = new Vue({el: '#edit',data:"+sb+");");
+        System.out.println("let vueCheck = new Vue({el: '#check',data:"+sb+");");
+//        System.out.println();
+//        String ob = "let url = " + "'/"+name+"';";
+//        String idUrl = "let idUrl = url + '/' + id;";
+//        System.out.println(ob);
+//        System.out.println(idUrl);
 
-//        System.out.println(sb);
+        System.out.println("\n//保存");
+        System.out.println("function save(id) {" +
+                "\n\tlet url = '/" + name+"';"+
+                "\n\t//layer.open" +
+                "\n\tsaveVue(url,vueSave,layer.close(index));\n}");
+
+        System.out.println("\n//查看");
+        System.out.println("function check(id) {" +
+                "\n\tlet url = '/" + name+"/'+id;"+
+                "\n\tcheckVue(url,vueCheck,openCheck());" +
+                "\n\tfunction openCheck(){" +
+                "\n\t\t//layer.open" +
+                "\n\t}" +
+                "\n}");
+
+        System.out.println("\n//编辑");
+        System.out.println("function edit(id) {" +
+                "\n\tlet url = '/" + name+"/'+id;"+
+                "\n\tcheckVue(url,vueEdit,openEdit());" +
+                "\n\tfunction openEdit(){" +
+                "\n\t\t//layer.open" +
+                "\n\t\teditVue(url,vueEdit,layer.close(index));\n\t}\n}");
+
+        System.out.println("\n//删除");
+        System.out.println("function del(id) {" +
+                "\n\tlet url = '/" + name+"/'+id;"+
+                "\n\t//layer.confirm" +
+                "\n\tdelVue(url,layer.close(index))\n}");
+
     }
 
     public static String toLowerCaseFirstOne(String s){
